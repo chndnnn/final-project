08674a615e14893e0ec93ee5449e25cab8ae9439
Chndnnn <chndn136@gmail.com>
2023-01-19 10:57:14 +0530

    Made chat message  test

---
+++ frontend/src/components/TestTest.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          :text="['hey, how are you? Chandan this side']"
+          sent
+        />
+        <q-chat-message
+          :text="['doing fine, how r you?']"
+        />
+      </div>
+    </div>
+  </template>
